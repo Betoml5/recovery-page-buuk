f5f5d774581623e3cc9237803b87edbcb34a65a5
Betoml5 <betoml5@hotmail.com>
2022-05-12 16:07:14 -0500

    Fix submit to api

--- index.js
+++ index.js
@@ -1,46 +1,50 @@
 const btn = document.querySelector("#btn");
-const API_URL = "https://buukapi.herokuapp.com/api/v1"
-
+const API_URL = "http://localhost:3080/api/v1";
 
 async function changePassword() {
-    const password = document.querySelector("#password");
-    const confirmPassword = document.querySelector("#confirmpassword")
-    const errorMsg = document.querySelector("#errormsg")
-    const successMsg = document.querySelector("#successmsg")
-    let hasError = false;
-    errorMsg.innerHTML = "";
+  const password = document.querySelector("#password");
+  const confirmPassword = document.querySelector("#confirmpassword");
+  const errorMsg = document.querySelector("#errormsg");
+  const successMsg = document.querySelector("#successmsg");
+  let hasError = false;
+  errorMsg.innerHTML = "";
 
-    if (password.value === "" || confirmPassword.value === "") {
-        errorMsg.innerHTML += "<p>Hay algunos campos sin completar</p>";
-        hasError = true;
+  if (password.value === "" || confirmPassword.value === "") {
+    errorMsg.innerHTML += "<p>Hay algunos campos sin completar</p>";
+    hasError = true;
+  }
 
+  if (password.value !== confirmPassword.value) {
+    errorMsg.innerHTML += `<p>Las contraseñas no coinciden </p>`;
+    hasError = true;
+  }
 
+  if (hasError === false) {
+    try {
+      const response = await fetch(`${API_URL}/auth/forgot-password`, {
+        method: "POST",
+      });
+      const data = await response.json();
 
+      console.log(data);
+      successMsg.innerHTML = "<p>Contraseña actualizada correctamente</p>";
+      password.value = "";
+      confirmPassword.value = "";
+    } catch (error) {
+      errorMsg.innerHTML = "<p>Ocurrio un error inesperado en el servidor</p>";
     }
+  }
+  password.value = "";
+  confirmPassword.value = "";
 
-    if (password !== confirmPassword) {
-        errorMsg.innerHTML += `<p>Las contraseñas no coinciden </p>`;
-        hasError = true;
-    }
+  // const response = await fetch(`${API_URL}/forgot-password`);
+  // const data = await response.json();
+  // if (data) {
 
-    if (!hasError) {
-        successMsg.innerHTML = "<p>Contraseña actualizada correctamente</p>"
-        password.value = "";
-        confirmPassword.value = "";
-
-    }
-
-    // const response = await fetch(`${API_URL}/forgot-password`);
-    // const data = await response.json();
-    // if (data) {
-
-    // }
-
+  // }
 }
 
 btn.addEventListener("click", (e) => {
-
-
-    e.preventDefault()
-    changePassword()
-})
+  e.preventDefault();
+  changePassword();
+});
