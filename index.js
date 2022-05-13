a3ff64c4412cda7bd56d09fcd7f0d486f4574a77
Betoml5 <betoml5@hotmail.com>
2022-05-13 08:27:56 -0500

    Change api endpoint

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const btn = document.querySelector("#btn");
 const API_URL = "http://localhost:3080/api/v1";
 
+var url_string = window.location.href; //window.location.href
+var url = new URL(url_string);
+//Este token tiene una expiracion de 15 min
+var token = url.searchParams.get("token");
+
 async function changePassword() {
   const password = document.querySelector("#password");
   const confirmPassword = document.querySelector("#confirmpassword");
@@ -22,7 +27,7 @@ async function changePassword() {
 
   if (hasError === false) {
     try {
-      const response = await fetch(`${API_URL}/auth/forgot-password`, {
+      const response = await fetch(`${API_URL}/user/update`, {
         method: "POST",
       });
       const data = await response.json();
